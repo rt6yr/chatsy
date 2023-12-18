4fe13bbcfb7affd4755e5c6753e61da24baca6f9
rt6yr <150139361+rt6yr@users.noreply.github.com>
2023-12-18 14:37:14 +0530

    Update route.ts

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -4,8 +4,6 @@ import { getServerSideConfig } from "../../config/server";
 
 const serverConfig = getServerSideConfig();
 
-// Danger! Do not hard code any secret value here!
-// 警告！不要在这里写入任何敏感信息！
 const DANGER_CONFIG = {
   needCode: serverConfig.needCode,
   hideUserApiKey: serverConfig.hideUserApiKey,
