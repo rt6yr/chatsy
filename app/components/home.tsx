ffd746cecc4e46770648ac75e9a67955804e1046
rt6yr <rt6yrru+github@gmail.com>
2024-11-12 17:49:23 +0530

    chatsy 1.1

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -5,7 +5,7 @@ require("../polyfill");
 import { useState, useEffect } from "react";
 import styles from "./home.module.scss";
 
-import BotIcon from "../icons/bot.svg";
+import BotIcon from "../icons/bot.avif";
 import LoadingIcon from "../icons/three-dots.svg";
 
 import { getCSSVar, useMobileScreen } from "../utils";
